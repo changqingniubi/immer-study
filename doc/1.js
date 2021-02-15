5d5cf53d29b162eef0c4669c7ec7f6b49c2eea94
changqing <qing.chang@eeoa.com>
2021-02-15 12:31:36 +0800

    immer的使用与原理

---
+++ doc/1.js
@@ -0,0 +1,4 @@
+let objA = { name: 'zhufeng' };
+let objB = objA;
+objB.name = 'jiagou';
+console.log(objA.name);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//import {produce } from 'immer';
+import { useImmerState,produce } from './immer';
+
+// let baseState = {
+//   ids: [1],
+//   pos: {
+//     x: 1,
+//     y: 1 
+//   }
+// }
+
+// let nextState = produce(baseState, (draft) => {
+//   draft.ids.push(2);
+// })
+// console.log(baseState.ids === nextState.ids);//false
+// console.log(baseState.pos === nextState.pos);//true
+
+
+let id = 1;
+function Todos() {
+  const [todos, setTodos] = useImmerState({
+    list: []
+  })
+  const addTodo = () => setTodos((draft) => {
+    draft.list.push(id++)
+  })
+  return (
+    <>
+      <button onClick={addTodo}>增加</button>
+      <ul>
+        {
+          todos.list.map((item, index) => <li key={index}>{item}</li>)
+        }
+      </ul>
+    </>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Todos />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
